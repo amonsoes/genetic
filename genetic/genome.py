a1bb9d7e1fac9c9d2a524f337de363fd570faafc
soaresdesouza <amonsoes@gmail.com>
2021-10-11 18:56:01 +0200

    change epoch nums

--- genetic/genome.py
+++ genetic/genome.py
@@ -107,7 +107,7 @@ class LSTMGenome(Genome):
         'batch_size': [1, 2, 4, 8, 16],
         'num_layers': [1,2,3],
         'dropout': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
-        'epochs': [1,2,3,4,5,6,7,8],
+        'epochs': [5,6,7,8],
     }
 
     @classmethod
